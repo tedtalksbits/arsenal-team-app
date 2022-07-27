a96461dbe5a3d2d58a7ac99f79f3184d75f48e02
tedtalksbits <tedaneblake@gmail.com>
2022-07-26 20:55:25 -0400

    feat: add modal to display player, update color scheme

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,79 +1,81 @@
-import React, { useEffect, useState } from 'react'
-import { Scorecard, TeamName, TeamRow, TeamScore } from '../components/ScoreCard';
-import { MainHeader } from '../components/Header';
-import loadingImg from '../images/spin.svg'
-//http://api.football-data.org/v2/teams/759/matches
+import React, { useCallback, useEffect, useState } from "react";
+import { MainHeader } from "../components/Header";
+import loadingImg from "../images/spin.svg";
+import Matches from "../components/Matches";
 const HomePage = () => {
+    const [matches, setMatches] = useState([]);
+    const [isLoading, setisLoading] = useState(true);
+    const fetchMatches = useCallback(async () => {
+        const response = await fetch(`${process.env.REACT_APP_MATCHES_URL}`, {
+            method: "GET",
+            headers: {
+                "X-Auth-Token": "8c5b41465c1b4aa4bacb9a446dc74bff",
+            },
+        });
+        const result = await response.json();
+        // console.log(result.matches);
+        setMatches(result.matches.sort(sortByDate));
+        setisLoading(false);
+    }, []);
+    useEffect(() => {
+        fetchMatches();
+    }, [fetchMatches]);
 
-   useEffect(() => {
+    //  const sortByDate = (a, b) => {
+    //      const itemA = a.utcDate;
+    //      const itemB = b.utcDate;
 
-      fetchMatches();
-   }, []);
+    //      let comparison = 0;
 
-   const [matches, setMatches] = useState([]);
-   const [isLoading, setisLoading] = useState(true);
-   const fetchMatches = async () => {
-      const response = await fetch("http://api.football-data.org/v2/teams/57/matches", {
-         method: 'GET',
-         headers: {
-            'X-Auth-Token': '8c5b41465c1b4aa4bacb9a446dc74bff'
-         }
-      })
-      const result = await response.json();
-      // console.log(result.matches);
-      setMatches(result.matches.sort(sortByDate));
-      setisLoading(false);
+    //      itemA > itemB ? (comparison = 1) : (comparison = -1);
 
-   }
+    //      return comparison * -1;
+    //  };
+    const sortByDate = (a, b) => {
+        const itemA = new Date(a.utcDate);
+        const itemB = new Date(b.utcDate);
 
-   const sortByDate = (a, b) => {
-      const itemA = a.utcDate;
-      const itemB = b.utcDate;
+        let comparison = 0;
 
-      let comparison = 0;
+        itemA > itemB ? (comparison = 1) : (comparison = -1);
 
-      itemA > itemB ? comparison = 1 : comparison = -1;
+        return comparison * 1;
+    };
 
-      return comparison * -1;
-   }
+    const upcomingMatches = matches.filter(
+        (match) => match.score.fullTime.homeTeam === null
+    );
+    const pastMatches = matches.filter(
+        (match) => match.score.fullTime.homeTeam !== null
+    );
 
-   return (
-      <MainHeader >
-
-         {isLoading ?
+    if (isLoading) {
+        return (
             <div className="loading">
-               <div className="loading-content">
-                  <h1>Loading...</h1>
-                  <img src={loadingImg} alt="loading" />
-               </div>
-            </div> :
+                <div className="loading-content">
+                    <h1>Loading...</h1>
+                    <img src={loadingImg} alt="loading" />
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <MainHeader>
             <>
-               <h1>recent matches</h1>
-               <div className="container scores-container">
-
-                  {matches.map((match) => (
-
-                     match.score.fullTime.homeTeam === null ?
-                        null
-                        :
-                        <Scorecard className="match-card" key={match.id}>
-                           <TeamRow>
-                              <TeamName>{match.homeTeam.name}</TeamName>
-                              <TeamScore>{match.score.fullTime.homeTeam}</TeamScore>
-                           </TeamRow>
-                           <TeamRow>
-                              <TeamName>{match.awayTeam.name}</TeamName>
-                              <TeamScore>{match.score.fullTime.awayTeam}</TeamScore>
-                           </TeamRow>
-
-                        </Scorecard>
-
-                  ))}
-               </div>
+                <h1>
+                    Arsenal {new Date().getFullYear()}/
+                    {new Date().getFullYear() + 1} schedule
+                </h1>
+                <Matches matches={upcomingMatches}>
+                    <span className="matches-dropdown">Upcoming Matches</span>
+                </Matches>
+                <Matches matches={pastMatches}>
+                    <span className="matches-dropdown">Recent Matches</span>
+                </Matches>
             </>
-         }
-      </MainHeader>
-   )
-}
+        </MainHeader>
+    );
+};
 
-export default HomePage
+export default HomePage;
